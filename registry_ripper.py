eb590f6e7c5cf5f2f9fb3a6046dcb5927db855a8
nanoSpl0it <nanosploit@gmail.com>
2014-06-17 22:41:43 -0700

    Update registry_ripper.py

--- registry_ripper.py
+++ registry_ripper.py
@@ -2,7 +2,7 @@
 
 """
 -------------------------------------------------------------------------------
-Copyright 2014 CaptainCrabnasty
+Copyright 2014 nanoSpl0it
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
